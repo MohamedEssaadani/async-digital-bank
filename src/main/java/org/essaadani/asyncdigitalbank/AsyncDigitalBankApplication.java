fef7313fe9a8d8e3c7d45f886f57ce0a48263048
ES-SAADANI Mohamed <mohamed.essaadani@adria-bt.com>
2024-03-29 15:13:21 +0000

    first commit

---
+++ src/main/java/org/essaadani/asyncdigitalbank/AsyncDigitalBankApplication.java
@@ -0,0 +1,13 @@
+package org.essaadani.asyncdigitalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncDigitalBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncDigitalBankApplication.class, args);
+    }
+
+}
